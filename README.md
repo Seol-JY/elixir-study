edd25a0138a958d2ef633dd76e255aeaa19bd3e0
Seol_JY <wlsdud5654@gmail.com>
2023-01-14 17:35:41 +0900

    [Study]

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ Elixir 언어, Phoenix 프레임워크 찍먹
 
 | link                                                                                                                                |
 | ----------------------------------------------------------------------------------------------------------------------------------- |
+| [엘릭서(Elixir)와 웹 프레임워크 피닉스(Phoenix) 강좌](https://www.bitzflex.com/)                                                    |
 | [Elixir 공식 홈페이지](https://elixir-lang.org/)                                                                                    |
 | [Elixir 한국어 튜토리얼](https://elixirschool.com/ko)                                                                               |
 | [그럼에도 불구하고 Elixir: 카카오워크 서버팀의 Elixir 실용주의 프로그래밍 / if(kakao)2022](https://youtu.be/NotXpRovDoA)            |
 | [liftIO 2022 : 개발자 부족의 시대, Elixir가 특효약이 될수 있는 이유 - 한국축산데이터 CTO Max(이재철)](https://youtu.be/lAaD-6OQSHE) |
+| [엘릭서(Elixir)와 웹 프레임워크 피닉스(Phoenix) 강좌](https://www.bitzflex.com/)                                                    |
 
 ## 사용 방법 - macOS 기준
 
--- simple.exs
+++ simple.exs
@@ -27,3 +27,47 @@ IO.puts(1.0e-10)
 # round
 round(3.58) |> IO.puts()   # 4
 trunc(3.58) |> IO.puts()   # 3
+
+# atom
+# atom? => c에서의 define처럼 의미를 분명하게 하기 위해 정의해서 사용할 수 있는 워드를 말함,
+# Elixir에서는 true와 false도 그저 atom임 < :a는 애텀이고, a는 변수임 >
+:foo |> IO.puts()            # foo
+(:foo == :bar) |> IO.puts()  # false
+is_atom(true) |> IO.puts()   # true
+is_boolean(:true) |> IO.puts()  # true
+(:true === true) |> IO.puts()  # true
+
+is_atom(MyApp.MyModule) |> IO.puts() # true   , 모듈 이름 역시 하나의 Atom, 영어 대문자로 시작하고 .있으면 Atom으로 본다
+
+# 문자열은 큰 따옴표로 표현한다.
+
+# 논리 연산에서 ||, &&, !는 모든 타입에서 지원
+(-20 || true) |> IO.puts() # -20
+(false || 42) |> IO.puts() # 42
+(42 && true)  |> IO.puts() # true
+(!42)         |> IO.puts() # false
+(!false)      |> IO.puts() # true
+
+# and, or, not 연산에서는 반드시 첫번째 인자가 true나 false여야만 함
+(true and 42) |> IO.puts()
+# 애러: 42 and true
+# 애러: not 42
+
+#  ==, !=, ===, !==, <=, >=, <, > 와 같은 비교연산도 가능
+# 정수와 실수 타입까지 엄격하게 비교할 때에는 === 사용
+(1 > 2) |> IO.puts() # false
+(1 != 2) |> IO.puts() # true
+
+(2 == 2.0) |> IO.puts()  # true
+(2 === 2.0) |> IO.puts()  # false
+
+# 타입과 관계없이 비교 연산을 수행할 수 있다는 점이 중요
+# 숫자 < 애텀 < 참조 < 함수 < 포트 < pid < 튜플 < 맵 < 리스트 < 비트스트링
+(:hello > 999) |> IO.puts() # true
+({:hello, :world} > [1, 2, 3]) |> IO.puts() # false
+
+# 문자열 내부 식 전개
+name = "Sean"
+"Hello #{name}" |> IO.puts()  # "Hello Sean"
+# 문자열 합치기 "<>"
+"Hello " <> name |> IO.puts() # "Hello Sean"
